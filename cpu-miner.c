bb93b2b032a3aab651a0be4298ffb538b3b1c0a4
zcdk077 <zackynautika@gmail.com>
2025-01-25 21:15:48 +0700

    Update cpu-miner.c

--- cpu-miner.c
+++ cpu-miner.c
@@ -912,7 +912,6 @@ void scale_hash_for_display ( double* hashrate, char* prefix )
 static inline void sprintf_et( char *str, long unsigned int seconds )
 {
     long unsigned int min = seconds / 60;
-    long unsigned int min = seconds / 60;
     long unsigned int sec = seconds % 60;
     long unsigned int hrs = min / 60;
     
@@ -2665,31 +2664,30 @@ out:
 
 static void show_credits()
 {
-    printf(CL_LGR"   ###########   #####   ##      ##  ###  ##      #  #####   #### \n");
-    printf(CL_LGR"        #       #       # #    # #   #   # #     #  #      #    #\n");
-    printf(CL_LGR"       #       #       #  #  #  #   #   #  #    #  #      #    #\n");
-    printf(CL_LGR"      #       #       #   ##   #   #   #   #   #  #####  #####\n");
-    printf(CL_LGR"     #       #       #        #   #   #    #  #  #      # #\n");
-    printf(CL_LGR"    #       #       #        #   #   #     # #  #      #  #\n");
-    printf(CL_LGR"   #        #####  #        #  ###  #      ##  ###### #   ##\n");
+    printf(CL_LCY"   ###########   #####   ##      ##  ###  ##      #  #####   #### \n");
+    printf(CL_LCY"        #       #       # #    # #   #   # #     #  #      #    #\n");
+    printf(CL_LCY"       #       #       #  #  #  #   #   #  #    #  #      #    #\n");
+    printf(CL_LCY"      #       #       #   ##   #   #   #   #   #  #####  #####\n");
+    printf(CL_LCY"     #       #       #        #   #   #    #  #  #      # #\n");
+    printf(CL_LCY"    #       #       #        #   #   #     # #  #      #  #\n");
+    printf(CL_LCY"   #        #####  #        #  ###  #      ##  ###### #   ##\n");
     printf(CL_N"######################################################################\n\n");
-    printf(CL_RD2"                 ** MULTI ALGO FOR MINING **\n");
+    printf(CL_GRN"                        ** MULTI ALGO FOR MINING **\n");
     printf("\n");
-    printf(CL_LCY"              **"PACKAGE_NAME"" CL_YLW"" PACKAGE_VERSION"" CL_LCY" by zcdk077**\n");
-    printf(CL_YLW"   Based Originaly from cpuminer-multi by tpruvot and cpuminer-opt by JayDDee\n");
-    printf(CL_N"######################################################################\n\n");
+    printf(CL_LCY"                     ** "PACKAGE_NAME" " CL_YLW"" PACKAGE_VERSION"" CL_LCY" by zcdk077 **\n");
+    printf(CL_YLW"                 Based Originaly by tpruvot and JayDDee\n");
+    printf(CL_N"######################################################################\n");
     printf(CL_LCY"  Author  "CL_LGR"           : "CL_YLW"zcdk077\n");
     printf(CL_LCY"  Git repo"CL_LGR"           : "CL_YLW"https:" "/" "/" "github.com" "/" "zcdk077" "/" "TCMiner\n");
     printf(CL_LCY"  Original git repo"CL_LGR"  : "CL_YLW"https:" "/" "/" "github.com" "/" "tpruvot" "/" "cpuminer-multi\n");
-    printf(CL_LCY"  Original git repo"CL_LGR"  : "CL_YLW"https:" "/" "/" "github.com" "/" "JayDDee" "/" "cpuminer-opt\n");
-    printf(CL_N"########################## "CL_LCY"Donation zcdk077"CL_N" ##########################\n\n");
+    printf(CL_LCY"  Original git repo"CL_LGR"  : "CL_YLW"https:" "/" "/" "github.com" "/" "JayDDee" "/" "cpuminer-opt\n\n");
+    printf(CL_N"########################## "CL_LCY"Donation zcdk077"CL_N" ##########################\n");
     printf(CL_LCY"  DGB donation addr"CL_LGR"  : "CL_YLW"DRz9CYkQDmtUZUCT3YHR4i5giwhBcAAdva\n");
     printf(CL_LCY"  MBC donation addr"CL_LGR"  : "CL_YLW"mbc1qk3fej00mkksw9g4496ftm98dyg4m0ftegje6r8\n");
-    printf(CL_LCY" VRSC donation addr"CL_LGR"  : "CL_YLW"RGdgdAU7xB3vEwSfhPYGJJY9R85iAvhVtS\n");
-    printf(CL_N"########################## "CL_LCY"Donation tpruvot"CL_N" ##########################\n\n");
-    printf(CL_LCY"  BTC donation addr "CL_LGR" : "CL_YLW"1FhDPLPpw18X4srecguG3MxJYe4a1JsZnd\n");
-    printf(CL_N"########################## "CL_LCY"Donation JayDDee"CL_N" ##########################\n\n");
-    printf(CL_LCY"  BTC donation addr "CL_LGR" : "CL_YLW"12tdvfF7KmAsihBXQXynT6E6th2c2pByTT\n");
+    printf(CL_LCY" VRSC donation addr"CL_LGR"  : "CL_YLW"RGdgdAU7xB3vEwSfhPYGJJY9R85iAvhVtS\n\n");
+    printf(CL_N"############################## "CL_LCY"Donation"CL_N" ##############################\n");
+    printf(CL_LCY"  BTC donation addr "CL_LGR" : "CL_YLW"1FhDPLPpw18X4srecguG3MxJYe4a1JsZnd ( tpruvot )\n");
+    printf(CL_LCY"  BTC donation addr "CL_LGR" : "CL_YLW"12tdvfF7KmAsihBXQXynT6E6th2c2pByTT ( JayDDee )\n");
     printf(CL_N"######################################################################\n");
 }
 
