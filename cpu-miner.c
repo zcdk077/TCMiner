0af01b4b0b6a0f6e322a808e7fc6bde272fa4bec
zcdk077 <zackynautika@gmail.com>
2025-01-26 01:03:54 +0700

    Update cpu-miner.c

--- cpu-miner.c
+++ cpu-miner.c
@@ -2704,7 +2704,36 @@ out:
     return NULL;
 }
 
-#include "res/banner.h";
+
+static void show_credits()
+{
+    printf(CL_LCY"   ###########   #####   ##      ##  ###  ##      #  #####   #### \n");
+    printf(CL_LCY"        #       #       # #    # #   #   # #     #  #      #    #\n");
+    printf(CL_LCY"       #       #       #  #  #  #   #   #  #    #  #      #    #\n");
+    printf(CL_LCY"      #       #       #   ##   #   #   #   #   #  #####  #####\n");
+    printf(CL_LCY"     #       #       #        #   #   #    #  #  #      # #\n");
+    printf(CL_LCY"    #       #       #        #   #   #     # #  #      #  #\n");
+    printf(CL_LCY"   #        #####  #        #  ###  #      ##  ###### #   ##\n");
+    printf(CL_N"######################################################################\n\n");
+    printf(CL_RED"                     ** MULTI ALGO FOR MINING **\n");
+    printf("\n");
+    printf(CL_LCY"                     ** "PACKAGE_NAME" " CL_YLW"" PACKAGE_VERSION"" CL_LCY" by zcdk077 **\n");
+    printf(CL_YLW"                 Based Originaly by tpruvot and JayDDee\n");
+    printf(CL_N"######################################################################\n");
+    printf(CL_LCY"     Author  "CL_LGR"           : "CL_YLW"zcdk077\n");
+    printf(CL_LCY"     Git repo"CL_LGR"           : "CL_YLW"https:" "/" "/" "github.com" "/" "zcdk077" "/" "TCMiner\n");
+    printf(CL_LCY"     Original git repo"CL_LGR"  : "CL_YLW"https:" "/" "/" "github.com" "/" "tpruvot" "/" "cpuminer-multi\n");
+    printf(CL_LCY"     Original git repo"CL_LGR"  : "CL_YLW"https:" "/" "/" "github.com" "/" "JayDDee" "/" "cpuminer-opt\n\n");
+    printf(CL_N"########################## "CL_LCY"Donation zcdk077"CL_N" ##########################\n");
+    printf(CL_LCY"    DGB donation addr"CL_LGR"  : "CL_YLW"DRz9CYkQDmtUZUCT3YHR4i5giwhBcAAdva\n");
+    printf(CL_LCY"    MBC donation addr"CL_LGR"  : "CL_YLW"mbc1qk3fej00mkksw9g4496ftm98dyg4m0ftegje6r8\n");
+    printf(CL_LCY"   VRSC donation addr"CL_LGR"  : "CL_YLW"RGdgdAU7xB3vEwSfhPYGJJY9R85iAvhVtS\n\n");
+    printf(CL_N"############################## "CL_LCY"Donation"CL_N" ##############################\n");
+    printf(CL_LCY"  BTC donation addr "CL_LGR" : "CL_YLW"1FhDPLPpw18X4srecguG3MxJYe4a1JsZnd ( tpruvot )\n");
+    printf(CL_LCY"  BTC donation addr "CL_LGR" : "CL_YLW"12tdvfF7KmAsihBXQXynT6E6th2c2pByTT ( JayDDee )\n");
+    printf(CL_N"######################################################################\n");
+}
+
 
 #define check_cpu_capability() cpu_capability( false )
 #define display_cpu_capability() cpu_capability( true )
